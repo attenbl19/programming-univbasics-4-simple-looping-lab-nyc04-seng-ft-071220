8f469fddc6535e4165cf09398a18b97af622e0de
Lida Attenborough <attenboroughl@protonmail.com>
2020-07-16 14:53:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -34,6 +34,11 @@ def return_string_array(array)
   new_array = []
   
   while counter < array.length do
-    array[counter]
+    array[counter] = array[counter].to_s
+    counter +=1
+  end
+end
   
-  while 
+  
+  
+  
